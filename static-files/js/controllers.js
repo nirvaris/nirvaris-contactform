c1b2d31283d48f37822f5e56208af8c553d8ad4e
Cristiano Kurt Ritzke <cristianokr@Tiano-Mac.local>
2015-10-14 10:37:02 -0300

    form template and messages

--- static-files/js/controllers.js
+++ static-files/js/controllers.js
@@ -5,23 +5,19 @@
 var nirvarisControllers = angular.module('nirvarisApp', []);
 
 nirvarisControllers.controller('nirvarisCtrl', ['$scope', '$http', function($scope, $http) {
-	// your controller code here
-	console.log('I am here!');
+
 }]);
 
 
 nirvarisControllers.controller('ContactFormCtrl', ['$scope', '$http', function ($scope, $http, $sce) {
-	//$scope.keyword = '';
-	//$scope.results = [];
-
-
-	
-	$scope.sendContactMessage = function() {
+	$scope.sendContactMessage = function(form) {
 		$http.post('#', $.param({ 
 			name: angular.element(document.getElementsByName('name')[0]).val(),
 			email: angular.element(document.getElementsByName('email')[0]).val(),
 			message: angular.element(document.getElementsByName('message')[0]).val(),
 			send_to_me: document.getElementsByName('send_to_me')[0].checked,
+			anti_spam_token: angular.element(document.getElementsByName('anti_spam_token')[0]).val(),
+			anti_spam_hidden: angular.element(document.getElementsByName('anti_spam_hidden')[0]).val(),
 			csrfmiddlewaretoken: angular.element(document.getElementsByName('csrfmiddlewaretoken')[0]).val() 
 		}), {headers: {'Content-Type': 'application/x-www-form-urlencoded'}}).
 		then(function(response) {
