0df318654807a2729ea63cfa30bb8f29ec4c9567
JB <juliano.binder@gmail.com>
2015-10-21 11:56:44 +0100

    locale

--- contactform/urls.py
+++ contactform/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
 from .views import ContactFormView, ContactFormTag
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^form-tag$', ContactFormTag.as_view(), name='form-tag'),
     url(r'^$', ContactFormView.as_view(), name='contact'),
-]
+)
